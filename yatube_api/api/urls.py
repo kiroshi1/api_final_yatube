cdfb9da725b765136474991be42bd46ef5155aa5
Denis <razgonyaev1@yandex.ru>
2021-08-18 13:04:12 +0300

    test

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,20 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+from .views import CommentViewSet, GroupViewSet, PostViewSet, FollowViewSet
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments')
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,43 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .permissions import IsOwner
+from posts.models import Group, Post, Follow
+
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer, FollowSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwner, IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwner, IsAuthenticated]
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        queryset = post.comments.all()
+        return queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwner, IsAuthenticated]
+    serializer_class = FollowSerializer
+
